99753cf3eaef4b73bd703043e2c85a6646268e71
Jerin <jerinjacob@eloit.com>
2024-06-05 18:08:47 +0530

    Initial:

---
+++ course.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+
+// Variables
+// Variable - var
+// Constant - let
+
+var highScore = 39
+highScore = 30
+
+let name: String = "Jerin"
+
+// Arrays
+
+//var ages: Array<Double> = [23,526,14,6347,242]
+//
+//ages.append(5555559485295834)
+//ages.count
+//
+//ages.first
+//ages.last
+//ages[4]
+//
+//
+//print(ages)
+//ages.reverse()
+//print(ages)
+//ages.shuffle()
+//print(ages)
+
+// Sets
+// No duplicates
+
+var ages: Array<Double> = [23,526,14,6347,242, 242, 242, 242, 242]
+
+var setAges = Set(ages)
+
